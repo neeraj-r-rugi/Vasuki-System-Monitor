7f04d7f6ccad87e9cd28e904884b93261647285c
Neeraj R Rugi <texonace123@gmail.com>
2025-06-30 21:42:30 +0530

    Intial Executable created

--- src/main.c
+++ src/main.c
@@ -1,5 +1,12 @@
 #include "main.h"
 
 int main(){
+    char * test = "Hello Vasuki!\n";
+    printf("%s", test);
+    #ifdef _WIN32
+        system("timeout/ T3 > null");
+    #else
+        system("sleep 3");
+    #endif
     return 0;
 }
